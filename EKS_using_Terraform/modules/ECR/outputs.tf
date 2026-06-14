1dc2d35c2b332e9be468851438edf71c6b2a94eb
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-15 03:30:05 +0600

    simple updates in ecr

--- EKS_using_Terraform/modules/ECR/outputs.tf
+++ EKS_using_Terraform/modules/ECR/outputs.tf
@@ -1,3 +1,7 @@
-output "repository_url" {
-  value = aws_ecr_repository.app.repository_url
+output "frontend_repository_url" {
+  value = aws_ecr_repository.repos["frontend"].repository_url
+}
+
+output "backend_repository_url" {
+  value = aws_ecr_repository.repos["backend"].repository_url
 }
